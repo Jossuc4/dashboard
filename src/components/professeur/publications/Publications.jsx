0896c45265cce6214ce2593913f3312b0afc0d8e
Jossuc4 <larryjossuc4@gmail.com>
2024-11-06 11:16:45 +0300

    style dashboard professeur

--- src/components/professeur/publications/Publications.jsx
+++ src/components/professeur/publications/Publications.jsx
@@ -5,11 +5,46 @@ const Publications = ()=>{
         <div className="pub card">
             <h3>Publications</h3>
             <div className="publications ">
-                <div className="publication">Lorem5</div>
-                <div className="publication">Lorem5</div>
-                <div className="publication">Lorem5</div>
-                <div className="publication">Lorem5</div>
-                <div className="publication">Lorem5</div>
+                <div className="publication">
+                    <div className="profil">
+                        <div className="square"></div>
+                        <div className="name">Larry Jossuca</div>
+                    </div>
+                    <div className="comment">This platform is awesome</div>
+                    <div className="publish-date">2022/01/01 12:00</div>
+                </div>
+                <div className="publication">
+                    <div className="profil">
+                        <div className="square"></div>
+                        <div className="name">Larry Jossuca</div>
+                    </div>
+                    <div className="comment">This platform is awesome</div>
+                    <div className="publish-date">2022/01/01 12:00</div>
+                </div>
+                <div className="publication">
+                    <div className="profil">
+                        <div className="square"></div>
+                        <div className="name">Larry Jossuca</div>
+                    </div>
+                    <div className="comment">This platform is awesome</div>
+                    <div className="publish-date">2022/01/01 12:00</div>
+                </div>
+                <div className="publication">
+                    <div className="profil">
+                        <div className="square"></div>
+                        <div className="name">Larry Jossuca</div>
+                    </div>
+                    <div className="comment">This platform is awesome</div>
+                    <div className="publish-date">2022/01/01 12:00</div>
+                </div>
+                <div className="publication">
+                    <div className="profil">
+                        <div className="square"></div>
+                        <div className="name">Larry Jossuca</div>
+                    </div>
+                    <div className="comment">This platform is awesome</div>
+                    <div className="publish-date">2022/01/01 12:00</div>
+                </div>
             </div>
         </div>
     )
